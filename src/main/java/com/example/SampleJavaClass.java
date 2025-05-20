446415780a50c3dd3b6bb0e0f9c513ffb995b5b6
Olivier Teuliere <teuliere@amazon.com>
2025-05-20 08:56:52 +0000

    Initial version to reproduce the bug

---
+++ src/main/java/com/example/SampleJavaClass.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import lombok.NonNull;
+
+public class SampleJavaClass {
+    public String sampleMethod(@NonNull final String dummy) {
+        // Comment on the first line
+        return "sampleMethod() called!";
+    }
+}
